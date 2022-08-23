44e9d20d5eba373955b105b1e3cac80c9a31d139
Arthur Picht <picht@arthurpicht.de>
2022-08-23 21:01:09 +0200

    SessionStatusSO extension, ChangePasswordSO

--- src/main/java/org/mentalizr/serviceObjects/SessionStatusSO.java
+++ src/main/java/org/mentalizr/serviceObjects/SessionStatusSO.java
@@ -5,17 +5,27 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class SessionStatusSO {
 
-    private boolean valid;
+    public static final String STATUS_VALID = "VALID";
+    public static final String STATUS_INVALID = "INVALID";
+    public static final String STATUS_INTERMEDIATE = "INTERMEDIATE";
+    public static final String REQUIRE_POLICY_CONSENT = "POLICY_CONSENT";
+    public static final String REQUIRE_RENEW_PASSWORD = "RENEW_PASSWORD";
+    public static final String REQUIRE_EMAIL_CONFIRMATION = "EMAIL_CONFIRMATION";
+    public static final String REQUIRE_2FA = "2FA";
+
+    private String status;
     private String userRole;
     private String sessionId;
+    private String require;
 
     public SessionStatusSO() {
     }
 
-    public SessionStatusSO(boolean valid, String userRole, String sessionId) {
-        this.valid = valid;
+    public SessionStatusSO(String status, String userRole, String sessionId, String require) {
+        this.status = status;
         this.userRole = userRole;
         this.sessionId = sessionId;
+        this.require = require;
     }
 
     public String getUserRole() {
@@ -34,11 +44,20 @@ public class SessionStatusSO {
         this.sessionId = sessionId;
     }
 
-    public boolean isValid() {
-        return valid;
+    public String getStatus() {
+        return this.status;
     }
 
-    public void setValid(boolean valid) {
-        this.valid = valid;
+    public void setStatus(String status) {
+        this.status = status;
     }
+
+    public String getRequire() {
+        return this.require;
+    }
+
+    public void setRequire(String require) {
+        this.require = require;
+    }
+
 }
