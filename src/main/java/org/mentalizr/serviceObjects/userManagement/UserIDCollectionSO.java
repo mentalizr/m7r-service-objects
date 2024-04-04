ac1f557769b832acffca57f88438f57b3caf015f
Fabian Kalk <fabian.kalk@comrize.de>
2024-04-04 15:26:01 +0200

    [BE-4] updated backup command

---
+++ src/main/java/org/mentalizr/serviceObjects/userManagement/UserIDCollectionSO.java
@@ -0,0 +1,19 @@
+package org.mentalizr.serviceObjects.userManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserIDCollectionSO {
+    private List<String> collection;
+    public UserIDCollectionSO() {
+        this.collection = new ArrayList<>();
+    }
+
+    public List<String> getCollection() {
+        return collection;
+    }
+
+    public void setCollection(List<String> collection) {
+        this.collection = collection;
+    }
+}
