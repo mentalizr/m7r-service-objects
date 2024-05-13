718d9b5a400019a48fe96f345bcbef797688c025
Arthur Picht <picht@arthurpicht.de>
2024-05-13 16:35:06 +0200

    code review

--- src/main/java/org/mentalizr/serviceObjects/userManagement/ActivityStatusMessageSO.java
+++ src/main/java/org/mentalizr/serviceObjects/userManagement/ActivityStatusMessageSO.java
@@ -7,93 +7,96 @@ import java.util.UUID;
 @XmlRootElement
 public class ActivityStatusMessageSO {
 
-    public static final String id = "id";
+    public static final String ID = "id";
     public static final String TIMESTAMP = "timestamp";
     public static final String USER_ID = "userid";
     public static final String REST_ID = "restid";
     public static final String ROLE = "role";
     public static final String MESSAGE = "message";
 
-    private String idValue;
-    private Long timestampValue;
-    private String userIdValue;
-    private String restIdValue;
-    private String roleValue;
-    private String messageValue;
+    private String id;
+    private Long timestamp;
+    private String userId;
+    private String restId;
+    private String role;
+    private String message;
 
     public ActivityStatusMessageSO() {
-        this.idValue = UUID.randomUUID().toString();
-        this.timestampValue = 0L;
-        this.userIdValue = "";
-        this.restIdValue = "";
-        this.roleValue = "";
-        this.messageValue = "";
+        this.id = UUID.randomUUID().toString();
+        this.timestamp = 0L;
+        this.userId = "";
+        this.restId = "";
+        this.role = "";
+        this.message = "";
     }
 
-    public ActivityStatusMessageSO(Long timestamp, String userId,
-                                   String restId, String role, String message) {
-        this.idValue = UUID.randomUUID().toString();
-        this.timestampValue = timestamp;
-        this.userIdValue = userId;
-        this.restIdValue = restId;
-        this.roleValue = role;
-        this.messageValue = message;
+    public String getId() {
+        return id;
     }
 
-    public String getIdValue() {
-        return idValue;
-    }
-
-    public void setIdValue(String idValue) {
-        this.idValue = idValue;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public Long getTimestamp() {
-        return timestampValue;
+        return timestamp;
     }
 
-    public void setTimestamp(Long timestampValue) {
-        this.timestampValue = timestampValue;
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
     }
 
     public String getUserId() {
-        return userIdValue;
+        return userId;
     }
 
-    public void setUserId(String userIdValue) {
-        this.userIdValue = userIdValue;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getRestId() {
-        return restIdValue;
+        return restId;
     }
 
-    public void setRestId(String restIdValue) {
-        this.restIdValue = restIdValue;
+    public void setRestId(String restId) {
+        this.restId = restId;
     }
 
     public String getRole() {
-        return roleValue;
+        return role;
     }
 
-    public void setRole(String roleValue) {
-        this.roleValue = roleValue;
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getMessage() {
-        return messageValue;
+        return message;
     }
 
-    public void setMessage(String messageValue) {
-        this.messageValue = messageValue;
+    public void setMessage(String message) {
+        this.message = message;
     }
 
+    @Override
     public String toString() {
-        if(this.messageValue.isEmpty()) {
-            return String.format("%s _ %s", this.userIdValue, this.restIdValue.replace("/","-"));
+        if (this.message.isEmpty()) {
+            return String.format("%s _ %s", this.userId, this.restId.replace("/","-"));
         }
-        return String.format("%s _ %s _ %s", this.userIdValue, this.restIdValue.replace("/","-"), this.messageValue);
+        return String.format("%s _ %s _ %s", this.userId, this.restId.replace("/","-"), this.message);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ActivityStatusMessageSO that = (ActivityStatusMessageSO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 
 }
