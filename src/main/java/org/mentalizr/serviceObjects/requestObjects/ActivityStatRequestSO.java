0a6673ac5feed9d9ccba660b577bd990f95546c8
Arthur Picht <picht@arthurpicht.de>
2024-07-23 21:24:20 +0200

    ActivityStatRequestSO: extended

--- src/main/java/org/mentalizr/serviceObjects/requestObjects/ActivityStatRequestSO.java
+++ src/main/java/org/mentalizr/serviceObjects/requestObjects/ActivityStatRequestSO.java
@@ -1,12 +1,23 @@
 package org.mentalizr.serviceObjects.requestObjects;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ActivityStatRequestSO {
     private Long fromTimestamp;
     private Long untilTimestamp;
+    private Set<String> projects;
+    private boolean projectsIncludeMode;
+    private Set<String> programs;
+    private boolean programsIncludeMode;
 
     public ActivityStatRequestSO() {
         this.fromTimestamp = 0L;
         this.untilTimestamp = Long.MAX_VALUE;
+        this.projects = new HashSet<>();
+        this.projectsIncludeMode = false;
+        this.programs = new HashSet<>();
+        this.programsIncludeMode = false;
     }
 
     public Long getFromTimestamp() {
@@ -23,6 +34,38 @@ public class ActivityStatRequestSO {
 
     public void setUntilTimestamp(Long untilTimestamp) {
         this.untilTimestamp = untilTimestamp;
+    }
+
+    public Set<String> getProjects() {
+        return this.projects;
+    }
+
+    public void setProjects(Set<String> projects) {
+        this.projects = projects;
+    }
+
+    public boolean isProjectsIncludeMode() {
+        return this.projectsIncludeMode;
+    }
+
+    public void setProjectsIncludeMode(boolean projectsIncludeMode) {
+        this.projectsIncludeMode = projectsIncludeMode;
+    }
+
+    public Set<String> getPrograms() {
+        return this.programs;
+    }
+
+    public void setPrograms(Set<String> programs) {
+        this.programs = programs;
+    }
+
+    public boolean isProgramsIncludeMode() {
+        return this.programsIncludeMode;
+    }
+
+    public void setProgramsIncludeMode(boolean programsIncludeMode) {
+        this.programsIncludeMode = programsIncludeMode;
     }
 
 }
