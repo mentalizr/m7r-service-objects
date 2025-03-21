1b4774a1e2bfb8cae8ea9d317c1d0632062886ae
Arthur Picht <picht@arthurpicht.de>
2025-03-21 12:24:43 +0100

    AccessKeyGetExpiredActivatedSO

---
+++ src/main/java/org/mentalizr/serviceObjects/userManagement/AccessKeyGetExpiredActivatedSO.java
@@ -0,0 +1,15 @@
+package org.mentalizr.serviceObjects.userManagement;
+
+public class AccessKeyGetExpiredActivatedSO {
+
+    private long lastUsedBefore;
+
+    public long getLastUsedBefore() {
+        return lastUsedBefore;
+    }
+
+    public void setLastUsedBefore(long lastUsedBefore) {
+        this.lastUsedBefore = lastUsedBefore;
+    }
+
+}
